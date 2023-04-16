df456fea05acde4d1b236157e4a1cae21df8ab0a
Toru Choi <blueocean239@gmail.com>
2023-04-16 18:11:06 +0900

    feat: wip, following codelab, for status

--- app/src/main/java/kr/toru/firstjetpack/InitActivity.kt
+++ app/src/main/java/kr/toru/firstjetpack/InitActivity.kt
@@ -1,29 +1,24 @@
 package kr.toru.firstjetpack
 
-import android.graphics.fonts.FontStyle
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kr.toru.firstjetpack.ui.theme.JetpackComposeTheme
 
 /*
@@ -34,35 +29,58 @@ class InitActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetToYou("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxWidth())
             }
         }
     }
 
     @Composable
     fun GreetToYou(name: String) {
-        Text(
-            text = "Hello $name!!",
-            color = Color.White,
-            fontFamily = FontFamily.Serif,
-            fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
-            fontSize = 30.sp,
-            modifier = Modifier.fillMaxWidth()
-                .wrapContentWidth(align = Alignment.Start)
-                .wrapContentHeight(align = Alignment.Top)
-                .background(Color.Cyan)
-                .padding(16.dp)
-        )
+        val expanded = remember { mutableStateOf(false) }
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                    Text(
+                        text = "Hello,",
+                        color = Color.White
+                    )
+                    Text(
+                        text = "$name!!",
+                        color = Color.White
+                    )
+                }
+                ElevatedButton(
+                    onClick = {
+                        expanded.value = !expanded.value
+                    }
+                ) {
+                    Text(text = if (expanded.value )"Show less" else "Show more")
+                }
+            }
+        }
     }
 
-    @Preview(showBackground = true)
+    @Composable
+    private fun MyApp(
+        modifier: Modifier = Modifier,
+        names: List<String> = listOf("World", "Compose")
+    ) {
+        Column(modifier = modifier) {
+            for (name in names) {
+                GreetToYou(name = name)
+            }
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 320)
     @Composable
     fun DefaultPreview() {
-        GreetToYou(name = "Android!!")
+        JetpackComposeTheme {
+            MyApp()
+        }
     }
 }
